1e51e8cd24d9c30437762ebe844112fd3f0e5578
user139405 <user139405@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-04-09 17:42:19 -0400

    initial setup

--- HWS_Project4/ViewController.swift
+++ HWS_Project4/ViewController.swift
@@ -1,18 +1,10 @@
-//
-//  ViewController.swift
-//  HWS_Project4
-//
-//  Created by user139405 on 4/9/19.
-//  Copyright © 2019 kabishau. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
